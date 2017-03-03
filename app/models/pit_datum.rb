79d377707a3c9e36343b0a4f53e27a493a180f76
Maxim Salikhov <msalihov@gmail.com>
2017-03-03 00:11:47 -0500

    Actually add team number to export

--- app/models/pit_datum.rb
+++ app/models/pit_datum.rb
@@ -6,17 +6,17 @@ class PitDatum < ApplicationRecord
 
   def self.to_csv(team, event)
     data = all.where(team_id: team, event_id: event).order(number: :asc)
-    columns = []
+    columns = ['Team number']
     eval(Team.find(team).scout_schema.pit_data).each do |section|
       section[1].each do |field|
-        columns.push(field[0].gsub('_', '').capitalize)
+        columns.push(field[0].gsub('_', ' ').capitalize)
       end
     end
     CSV.generate(headers: true) do |csv|
       csv << columns
       data.each do |entry|
         raw_data = eval(entry.data)
-        processed = []
+        processed = [entry.number.to_s]
         raw_data.each do |rd|
           processed.push(rd[1])
         end
